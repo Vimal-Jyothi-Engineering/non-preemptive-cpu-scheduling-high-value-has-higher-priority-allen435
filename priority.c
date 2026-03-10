3a421f48d6d28f578be70e2763b670b3c299836b
allen saji <allensaji023@gmail.com>
2026-03-10 20:43:30 +0530

    Update priority.c
    assignment 2

--- priority.c
+++ priority.c
@@ -17,15 +17,47 @@ int main()
     for(int i=0;i<n;i++)
     {
         scanf("%s %d %d %d",p[i].pid,&p[i].at,&p[i].bt,&p[i].pr);
-        p[i].done=0;
+        p[i].done = 0;
     }
 
-   .
-  ..
-     ..
-     ...
-     ..
-     ..
+    int completed = 0, time = 0;
+    float avg_wt = 0, avg_tat = 0;
+
+    while(completed < n)
+    {
+        int idx = -1;
+        int max_pr = -1;
+
+        for(int i=0;i<n;i++)
+        {
+            if(p[i].at <= time && p[i].done == 0)
+            {
+                if(p[i].pr > max_pr)
+                {
+                    max_pr = p[i].pr;
+                    idx = i;
+                }
+            }
+        }
+
+        if(idx != -1)
+        {
+            time += p[idx].bt;
+            p[idx].ct = time;
+            p[idx].tat = p[idx].ct - p[idx].at;
+            p[idx].wt = p[idx].tat - p[idx].bt;
+
+            avg_wt += p[idx].wt;
+            avg_tat += p[idx].tat;
+
+            p[idx].done = 1;
+            completed++;
+        }
+        else
+        {
+            time++;
+        }
+    }
 
     printf("Waiting Time:\n");
     for(int i=0;i<n;i++)
